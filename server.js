98bbbc2654014c9800f03ab94188cfef4dc029eb
Za Nya Hmue <113910682+zanya98@users.noreply.github.com>
2022-09-21 22:48:02 +0630

    Add files via upload

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
 });
 
-app.use(express.static("C:\Users\zanya\OneDrive\Desktop\NCC_JS_Class"));
+app.use(express.static("C:/Users/zanya/OneDrive/Desktop/NCC_JS_Class"));
 
 app.get("/", (req, res) => {
-  res.sendFile("C:\Users\zanya\OneDrive\Desktop\NCC_JS_Class" + "/index.html");
+  res.sendFile("C:/Users/zanya/OneDrive/Desktop/NCC_JS_Class" + "/index.html");
 });
