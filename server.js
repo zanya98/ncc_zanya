d117918f43661d7fbe384d7ca85951129a5b9713
Za Nya Hmue <113910682+zanya98@users.noreply.github.com>
2022-09-21 22:28:46 +0630

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-const http = require('http');
+const express = require("express");
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 8080;
-const firstName = 'Za Nya';
-const lastName = 'Hmue';
-const birthYear = 1998;
-let age = 2022 - birthYear;
-console.log(`My year old is ${age}`);
-console.log(`I'm ${firstName} ${lastName}.`);
-
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-
-  res.end (`I am ${firstName} ${lastName}.`);
-
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
